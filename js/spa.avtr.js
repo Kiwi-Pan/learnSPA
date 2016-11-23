788b2d0e4d62d06f4865b152d32d9c3f60c676fc
kiwi <lzch5490228@163.com>
2016-11-23 17:40:04 +0800

    first verson

---
+++ js/spa.avtr.js
@@ -0,0 +1,15 @@
+/*
+    spa.avtr.js
+    Avatar feature module for SPA
+*/
+
+/*jslint    browser: true, continue: true, 
+    devel: true, indent: 4, maxerr: 50,
+    newcap: true, nomen: true, plusplus: true,
+    regexp: true, sloppy: true, vars: false.
+    white: true
+*/
+
+/* global $, spa */
+
+spa.avtr = ( function() { return{}; }() )
